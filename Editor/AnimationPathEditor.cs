974b3072cc5db1456444cfe1aeba62f7c86b5aa2
Bartlomiej Wołk <bartlomiejwolk@gmail.com>
2015-01-29 14:22:41 +0100

    Remove AnimationPathHandles class

--- Editor/AnimationPathEditor.cs
+++ Editor/AnimationPathEditor.cs
@@ -15,16 +15,6 @@ namespace ATP.AnimationPathTools {
     [CustomEditor(typeof(AnimationPath))]
     public class AnimationPathEditor : Editor {
 
-        #region Composite Object
-
-        /// <summary>
-        /// Object responsible for drawing on-scene handles.
-        /// </summary>
-        private AnimationPathHandles animationPathHandles =
-            ScriptableObject.CreateInstance<AnimationPathHandles>();
-
-        #endregion Composite Object
-
         #region Keyboard Keys
 
         /// <summary>
@@ -371,7 +361,7 @@ namespace ATP.AnimationPathTools {
             Action<int> callbackHandler = DrawAddNodeButtonsCallbackHandler;
 
             // Draw add node buttons.
-            animationPathHandles.DrawAddNodeButtons(
+            DrawAddNodeButtons(
                 nodePositions,
                 callbackHandler,
                 addButtonStyle);
@@ -390,7 +380,7 @@ namespace ATP.AnimationPathTools {
 
             // Draw label for Movement mode.
             if (!script.TangentMode) {
-                animationPathHandles.DrawLabelForEachNode(
+                DrawLabelForEachNode(
                     nodes,
                     style,
                     "M",
@@ -401,7 +391,7 @@ namespace ATP.AnimationPathTools {
             }
             // Draw label for Tangent mode.
             else {
-                animationPathHandles.DrawLabelForEachNode(
+                DrawLabelForEachNode(
                     nodes,
                     style,
                     "T",
@@ -428,7 +418,7 @@ namespace ATP.AnimationPathTools {
                 DrawLinearTangentModeButtonsCallbackHandler;
 
             // Draw button.
-            animationPathHandles.DrawLinearTangentModeButtons(
+            DrawLinearTangentModeButtons(
                 nodePositions,
                 buttonStyle,
                 setNodeLinearCallback);
@@ -452,7 +442,7 @@ namespace ATP.AnimationPathTools {
                         moveDelta);
 
             // Draw handles.
-            animationPathHandles.DrawMovementHandles(nodes, handlerCallback);
+            DrawMovementHandles(nodes, handlerCallback);
         }
 
         /// <summary>
@@ -468,7 +458,7 @@ namespace ATP.AnimationPathTools {
                 Vector3[] nodes = script.GetNodePositions();
 
                 // Draw labels for all nodes.
-                animationPathHandles.DrawLabelForEachNode(
+                DrawLabelForEachNode(
                     nodes,
                     style,
                     "A",
@@ -485,7 +475,7 @@ namespace ATP.AnimationPathTools {
                 Vector3[] nodes = script.GetNodePositions();
 
                 // Draw labels for all nodes.
-                animationPathHandles.DrawLabelForEachNode(
+                DrawLabelForEachNode(
                     nodes,
                     style,
                     "S",
@@ -509,7 +499,7 @@ namespace ATP.AnimationPathTools {
                 DrawRemoveNodeButtonsCallbackHandles;
 
             // Draw add node buttons.
-            animationPathHandles.DrawRemoveNodeButtons(
+            DrawRemoveNodeButtons(
                 nodes,
                 removeNodeCallback,
                 removeButtonStyle);
@@ -531,7 +521,7 @@ namespace ATP.AnimationPathTools {
                 DrawSmoothTangentButtonsCallbackHandler;
 
             // Draw button.
-            animationPathHandles.DrawSmoothTangentButtons(
+            DrawSmoothTangentButtons(
                 nodePositions,
                 smoothButtonStyle,
                 smoothNodeCallback);
@@ -559,7 +549,7 @@ namespace ATP.AnimationPathTools {
             float[] speedValues = script.GetSpeedValues();
 
             // Draw speed labels.
-            animationPathHandles.DrawSpeedLabels(
+            DrawSpeedLabels(
                     nodePositions,
                     speedValues,
                     style);
@@ -579,7 +569,7 @@ namespace ATP.AnimationPathTools {
                 DrawTangentHandlesCallbackHandler;
 
             // Draw tangent handles.
-            animationPathHandles.DrawTangentHandles(
+            DrawTangentHandles(
                 nodes,
                 updateTangentsCallback);
         }
@@ -599,13 +589,359 @@ namespace ATP.AnimationPathTools {
             float[] timestamps = script.GetNodeTimestamps();
 
             // Draw timestamp labels.
-            animationPathHandles.DrawTimestampLabels(
+            DrawTimestampLabels(
                     nodes,
                     timestamps,
                     style);
         }
         #endregion DRAWING
+        #region Drawing methods
 
+        public void DrawAddNodeButtons(
+            Vector3[] nodePositions,
+            Action<int> callback,
+            GUIStyle buttonStyle) {
+
+            Handles.BeginGUI();
+
+            bool buttonPressed = false;
+
+            // Draw add buttons for each node. Execute callback on button
+            // press.
+            for (int i = 0; i < nodePositions.Length; i++) {
+                // Translate node's 3d position into screen coordinates.
+                Vector2 guiPoint = HandleUtility.WorldToGUIPoint(
+                        nodePositions[i]);
+
+                // Draw button.
+                buttonPressed = DrawButton(
+                    guiPoint,
+                    82,
+                    25,
+                    15,
+                    15,
+                    buttonStyle);
+
+                // Execute callback.
+                if (buttonPressed == true) {
+                    callback(i);
+                }
+            }
+
+            Handles.EndGUI();
+        }
+
+        public void DrawRemoveNodeButtons(
+            Vector3[] nodePositions,
+            Action<int> callback,
+            GUIStyle buttonStyle) {
+
+            Handles.BeginGUI();
+
+            bool buttonPressed = false;
+
+            // Draw add buttons for each node. Execute callback on button
+            // press.
+            for (int i = 0; i < nodePositions.Length; i++) {
+                // Translate node's 3d position into screen coordinates.
+                Vector2 guiPoint = HandleUtility.WorldToGUIPoint(
+                        nodePositions[i]);
+
+                // Draw button.
+                buttonPressed = DrawButton(
+                    guiPoint,
+                    64,
+                    25,
+                    15,
+                    15,
+                    buttonStyle);
+
+                // Execute callback.
+                if (buttonPressed == true) {
+                    callback(i);
+                }
+            }
+
+            Handles.EndGUI();
+        }
+
+        public void DrawMovementHandles(
+            Vector3[] nodes,
+            Action<int, Vector3, Vector3> callback) {
+
+            Vector3 newPos;
+
+            // For each node..
+            for (int i = 0; i < nodes.Length; i++) {
+                // draw node's handle.
+                newPos = Handles.PositionHandle(
+                        nodes[i],
+                        Quaternion.identity);
+
+                // If node was moved..
+                if (newPos != nodes[i]) {
+                    // Calculate movement delta.
+                    Vector3 moveDelta = newPos - nodes[i];
+
+                    // Execute callback.
+                    callback(i, newPos, moveDelta);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draw smooth tangent button for each node on the scene.
+        /// </summary>
+        /// <param name="curves">Animation curves.</param>
+        /// <param name="smoothWeight">
+        /// Weight parameter for the Unity's
+        /// <c>AnimationCurve.SmoothTangent</c> method.
+        /// </param>
+        /// <param name="smoothButtonStyle">Style of the button.</param>
+        /// <returns>If any button was pressed.</returns>
+        public void DrawSmoothTangentButtons(
+            Vector3[] nodePositions,
+            GUIStyle smoothButtonStyle,
+            Action<int> callback) {
+
+            Handles.BeginGUI();
+
+            bool buttonPressed;
+
+            // For each key..
+            for (int i = 0; i < nodePositions.Length; i++) {
+                // Translate node's 3d position into screen coordinates.
+                Vector2 guiPoint = HandleUtility.WorldToGUIPoint(
+                        nodePositions[i]);
+
+                // Create rectangle for the "+" button.
+                Rect rect = new Rect(
+                        guiPoint.x + 100,
+                        guiPoint.y + 25,
+                        15,
+                        15);
+
+                // Draw button.
+                buttonPressed = GUI.Button(rect, "", smoothButtonStyle);
+
+                // If button pressed..
+                if (buttonPressed == true) {
+                    // Execute callback.
+                    callback(i);
+                }
+            }
+
+            Handles.EndGUI();
+        }
+
+        /// <summary>
+        /// Draw linear tangent mode button for each node on the scene.
+        /// </summary>
+        /// <param name="curves">Animation curves.</param>
+        /// <param name="buttonStyle">Style of the button.</param>
+        /// <returns>If any button was pressed.</returns>
+        public void DrawLinearTangentModeButtons(
+            Vector3[] nodePositions,
+            GUIStyle buttonStyle,
+            Action<int> callback) {
+
+            Handles.BeginGUI();
+
+            bool buttonPressed;
+
+            // For each key..
+            for (int i = 0; i < nodePositions.Length; i++) {
+                // Translate node's 3d position into screen coordinates.
+                Vector2 guiPoint = HandleUtility.WorldToGUIPoint(
+                        nodePositions[i]);
+
+                // Create rectangle for the button.
+                Rect rect = new Rect(
+                        guiPoint.x + 120,
+                        guiPoint.y + 25,
+                        15,
+                        15);
+
+                // Draw button.
+                buttonPressed = GUI.Button(rect, "X", buttonStyle);
+
+                // If button pressed..
+                if (buttonPressed == true) {
+                    // Execute callback.
+                    callback(i);
+                }
+            }
+            Handles.EndGUI();
+        }
+
+        /// <summary>Draw speed label for each node on the scene.</summary>
+        /// <remarks>
+        /// Each node's speed value is the speed of a transform animated along
+        /// that path.
+        /// </remarks>
+        /// <param name="curves"></param>
+        /// <param name="labelStyle"></param>
+        public void DrawSpeedLabels(
+                Vector3[] nodePositions,
+                float[] speedValues,
+                GUIStyle labelStyle) {
+
+            Handles.BeginGUI();
+
+            // For each node.. First node has no speed label.
+            for (int i = 1; i < nodePositions.Length; i++) {
+                // Translate node's 3d position into screen coordinates.
+                Vector2 guiPoint = HandleUtility.WorldToGUIPoint(
+                        nodePositions[i]);
+
+                // Create rectangle for the label.
+                Rect rect = new Rect(guiPoint.x, guiPoint.y + 60, 40, 20);
+
+                // Label content.
+                string label = string.Format("{0:0.0} m/s", speedValues[i]);
+
+                // Draw label on the scene.
+                GUI.Box(
+                        rect,
+                        label,
+                        labelStyle);
+            }
+
+            Handles.EndGUI();
+        }
+
+        /// <summary>
+        /// For each node in the scene draw handle that allow manipulating
+        /// tangents for each of the animation curves separately.
+        /// </summary>
+        /// <returns>True if any handle was moved.</returns>
+        public void DrawTangentHandles(
+            Vector3[] nodes,
+            Action<int, Vector3> callback) {
+
+            // For each node..
+            for (int i = 0; i < nodes.Length; i++) {
+                // draw node's handle.
+                Vector3 newHandleValue = Handles.PositionHandle(
+                        nodes[i],
+                        Quaternion.identity);
+
+                // How much tangent's value changed in this frame.
+                Vector3 tangentDelta = newHandleValue - nodes[i];
+
+                // Remember if handle was moved.
+                if (tangentDelta != Vector3.zero) {
+                    // Execute callback.
+                    callback(i, tangentDelta);
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method allows displaying a string next to each node on the
+        /// scene.
+        /// </summary>
+        /// <param name="labelStyle">Label style.</param>
+        /// <param name="labelText">Label text.</param>
+        /// <param name="relativeXPos">
+        /// Horizontal label position relative to the node's position.
+        /// </param>
+        /// <param name="relativeYPos">
+        /// Vertical label position relative to the node's position.
+        /// </param>
+        /// <param name="width">Label width.</param>
+        /// <param name="height">Label height.</param>
+        public void DrawLabelForEachNode(
+            Vector3[] positions,
+            GUIStyle labelStyle,
+            string labelText,
+            int relativeXPos,
+            int relativeYPos,
+            int width,
+            int height) {
+
+            Handles.BeginGUI();
+
+            // For each node..
+            for (int i = 0; i < positions.Length; i++) {
+                // Translate node's 3d position into screen coordinates.
+                Vector2 guiPoint = HandleUtility.WorldToGUIPoint(
+                        positions[i]);
+
+                // Create rectangle for the "+" button.
+                Rect rect = new Rect(
+                        guiPoint.x + relativeXPos,
+                        guiPoint.y + relativeYPos,
+                        width,
+                        height);
+
+                // Draw label.
+                GUI.Label(rect, labelText, labelStyle);
+            }
+
+            Handles.EndGUI();
+        }
+
+        /// <summary>Draw information labels for each node.</summary>
+        /// <param name="curves">
+        /// Animation curves from which animation path is created.
+        /// </param>
+        /// <param name="labelStyle">
+        /// Style of the label displayed in the scene view.
+        /// </param>
+        public void DrawTimestampLabels(
+                Vector3[] nodes,
+                float[] timestamps,
+                GUIStyle labelStyle) {
+
+            // Draw GUI elements inside scene.
+            Handles.BeginGUI();
+
+            // For each node..
+            for (int i = 0; i < nodes.Length; i++) {
+                // Translate node's 3d position into screen coordinates.
+                Vector2 guiPoint = HandleUtility.WorldToGUIPoint(
+                        nodes[i]);
+
+                // Create rectangle for the label.
+                Rect rect = new Rect(guiPoint.x, guiPoint.y + 20, 40, 20);
+
+                // Label content.
+                string label = string.Format("{0:0.000}", timestamps[i]);
+
+                // Draw label on the scene.
+                GUI.Box(
+                        rect,
+                        label,
+                        labelStyle);
+            }
+
+            Handles.EndGUI();
+        }
+
+        public bool DrawButton(
+            Vector2 position,
+            int relativeXPos,
+            int relativeYPos,
+            int width,
+            int height,
+            GUIStyle style,
+            string buttonText = "") {
+
+            // Create rectangle for the "+" button.
+            Rect rectAdd = new Rect(
+                    position.x + relativeXPos,
+                    position.y + relativeYPos,
+                    width,
+                    height);
+
+            // Draw the "+" button.
+            bool addButtonPressed = GUI.Button(rectAdd, buttonText, style);
+
+            return addButtonPressed;
+        }
+
+        #endregion Drawing methods
         #region CALLBACK HANDLERS
 
         protected virtual void DrawAddNodeButtonsCallbackHandler(int nodeIndex) {

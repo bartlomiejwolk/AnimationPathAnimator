e5f8628e792b3e32e265a3c9ce75bd3aab7c5923
Bartlomiej Wołk <bartlomiejwolk@gmail.com>
2015-01-31 09:57:55 +0100

    Set default target path gizmo curve color

--- TargetAnimationPath.cs
+++ TargetAnimationPath.cs
@@ -1,5 +1,12 @@
-﻿namespace ATP.AnimationPathTools {
+﻿using UnityEngine;
+
+namespace ATP.AnimationPathTools {
 
     public class TargetAnimationPath : AnimationPath {
+
+        /// <summary>
+        /// Color of the gizmo curve.
+        /// </summary>
+        private Color gizmoCurveColor = Color.magenta;
     }
 }

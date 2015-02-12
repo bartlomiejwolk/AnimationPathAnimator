1f6eefd0481583f0d248211d4d40d40b2966dc45
Bartlomiej Wołk <bartlomiejwolk@gmail.com>
2015-02-12 17:53:48 +0100

    Create AnimationPathCurvesDebug class

---
+++ AnimationPathCurvesDebug.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using ATP.AnimationPathTools;
+
+[ExecuteInEditMode]
+public class AnimationPathCurvesDebug : MonoBehaviour {
+
+    private AnimationPathAnimator animator;
+    public AnimationCurve curveX;
+    public AnimationCurve curveY;
+    public AnimationCurve curveZ;
+
+    // Use this for initialization
+    void Awake() {
+    }
+
+    void OnEnable() {
+    }
+
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        animator = GetComponent<AnimationPathAnimator>();
+
+        curveX = animator.RotationCurves[0];
+        curveY = animator.RotationCurves[1];
+        curveZ = animator.RotationCurves[2];
+    }
+}

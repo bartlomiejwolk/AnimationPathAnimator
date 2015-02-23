35619ecf94f315ce1914693113abd7193405ce7d
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-02-23 11:03:42 +0100

    Import ReloadLevel script

---
+++ Libraries/ReloadLevel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReloadLevel : MonoBehaviour
+{
+		
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.R)) {
+			Reload ();
+		}
+	}
+
+	void Reload ()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}

0d1a8b02e21045f56723dc5fa090b1ac23c0d6fa
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-02-23 11:04:11 +0100

    Create PathData class

---
+++ PathData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ATP.AnimationPathTools {
+
+	public class PathData : ScriptableObject {
+
+		[SerializeField]
+		private AnimationPath animatedObjectPath;
+
+		[SerializeField]
+		private AnimationPath rotationPath;
+
+		[SerializeField]
+		private AnimationCurve easeCurve;
+
+		[SerializeField]
+		private AnimationCurve tiltingCurve;
+	}
+}

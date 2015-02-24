bbf46c4605581000764d57dcc105f28e275a43c5
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-02-24 10:37:50 +0100

    Add todo

--- Editor/AnimationPathBuilderEditor.cs
+++ Editor/AnimationPathBuilderEditor.cs
@@ -299,6 +299,7 @@ namespace ATP.AnimationPathTools {
         /// <summary>
         /// Record target object state for undo.
         /// </summary>
+        // TODO Remove and use undo inline.
         protected void HandleUndo() {
             Undo.RecordObject(Script.PathData.AnimatedObjectPath, "Change path");
         }

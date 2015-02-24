7057640aacb5bf51c55ab264a751168d1c24d97e
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-02-24 08:08:40 +0100

    Encapsulate fields

--- PathData.cs
+++ PathData.cs
@@ -30,42 +30,62 @@ namespace ATP.AnimationPathTools {
 	    }
 
 	    private void InitializeTiltingCurve() {
-	        tiltingCurve.AddKey(0, 0);
-	        tiltingCurve.AddKey(1, 0);
+	        TiltingCurve.AddKey(0, 0);
+	        TiltingCurve.AddKey(1, 0);
 	    }
 
 	    private void InitializeEaseCurve() {
-	        easeCurve.AddKey(0, DefaultEaseCurveValue);
-	        easeCurve.AddKey(1, DefaultEaseCurveValue);
+	        EaseCurve.AddKey(0, DefaultEaseCurveValue);
+	        EaseCurve.AddKey(1, DefaultEaseCurveValue);
 	    }
 
 	    protected virtual float DefaultEaseCurveValue {
             get { return 0.05f; }
 	    }
 
+	    public AnimationPath AnimatedObjectPath {
+	        get { return animatedObjectPath; }
+	        set { animatedObjectPath = value; }
+	    }
+
+	    public AnimationPath RotationPath {
+	        get { return rotationPath; }
+	        set { rotationPath = value; }
+	    }
+
+	    public AnimationCurve EaseCurve {
+	        get { return easeCurve; }
+	        set { easeCurve = value; }
+	    }
+
+	    public AnimationCurve TiltingCurve {
+	        get { return tiltingCurve; }
+	        set { tiltingCurve = value; }
+	    }
+
 	    private void InitializeRotationPath() {
             var firstNodePos = new Vector3(0, 0, 0);
-            rotationPath.CreateNewNode(0, firstNodePos);
+            RotationPath.CreateNewNode(0, firstNodePos);
 
             var lastNodePos = new Vector3(1, 0, 1);
-            rotationPath.CreateNewNode(1, lastNodePos);
+            RotationPath.CreateNewNode(1, lastNodePos);
 	    }
 
 	    private void InitializeAnimatedObjectPath() {
             var firstNodePos = new Vector3(0, 0, 0);
-            animatedObjectPath.CreateNewNode(0, firstNodePos);
+            AnimatedObjectPath.CreateNewNode(0, firstNodePos);
 
             var lastNodePos = new Vector3(1, 0, 1);
-            animatedObjectPath.CreateNewNode(1, lastNodePos);
+            AnimatedObjectPath.CreateNewNode(1, lastNodePos);
 	    }
 
 	    private void InstantiateReferenceTypes() {
-	        animatedObjectPath =
+	        AnimatedObjectPath =
 	            ScriptableObject.CreateInstance<AnimationPath>();
-	        rotationPath =
+	        RotationPath =
 	            ScriptableObject.CreateInstance<AnimationPath>();
-	        easeCurve = new AnimationCurve();
-	        tiltingCurve = new AnimationCurve();
+	        EaseCurve = new AnimationCurve();
+	        TiltingCurve = new AnimationCurve();
 	    }
 	}
 }

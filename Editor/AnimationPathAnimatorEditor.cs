28b4737ba0138016522d07aad77a3bfa6dbfc544
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-02-28 13:48:20 +0100

    Move const fields to another class

--- Editor/AnimationPathAnimatorEditor.cs
+++ Editor/AnimationPathAnimatorEditor.cs
@@ -105,6 +105,45 @@ namespace ATP.AnimationPathTools {
             get { return 0.25f; }
         }
 
+        public const KeyCode MoveAllKey = KeyCode.U;
+        public const KeyCode MoveSingleModeKey = KeyCode.Y;
+        public const KeyCode NoneModeShortcut = KeyCode.K;
+        public const KeyCode PlayPauseShortcut = KeyCode.Space;
+        public const KeyCode RotationModeShortcut = KeyCode.H;
+        public const KeyCode TiltingModeShortcut = KeyCode.J;
+        public const KeyCode UpdateAllShortcut = KeyCode.L;
+
+        public const KeyCode EaseModeShortcut = KeyCode.G;
+
+        /// <summary>
+        ///     Key shortcut to jump backward.
+        /// </summary>
+        public const KeyCode JumpBackward = KeyCode.LeftArrow;
+
+        /// <summary>
+        ///     Key shortcut to jump forward.
+        /// </summary>
+        public const KeyCode JumpForward = KeyCode.RightArrow;
+
+        /// <summary>
+        ///     Key shortcut to jump to the end of the animation.
+        /// </summary>
+        public const KeyCode JumpToEnd = KeyCode.UpArrow;
+
+        public const KeyCode JumpToNextNode = KeyCode.UpArrow;
+        public const KeyCode JumpToPreviousNode = KeyCode.DownArrow;
+
+        /// <summary>
+        ///     Key shortcut to jump to the beginning of the animation.
+        /// </summary>
+        public const KeyCode JumpToStart = KeyCode.DownArrow;
+
+        /// <summary>
+        ///     Keycode used as a modifier key.
+        /// </summary>
+        /// <remarks>Modifier key changes how other keys works.</remarks>
+        public const KeyCode ModKey = KeyCode.A;
+
         #endregion
 
         #region UNITY MESSAGES

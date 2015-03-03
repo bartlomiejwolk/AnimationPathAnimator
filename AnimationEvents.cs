c48c13c1e8ac5a066c8dd3aa489af44308a28ef4
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-03 11:12:57 +0100

    Display list in the inspector

--- AnimationEvents.cs
+++ AnimationEvents.cs
@@ -1,7 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+// TODO Move to separate file.
+[System.Serializable]
+public class NodeEvent {
+
+    
+
+}
 
 public class AnimationEvents : MonoBehaviour {
+
+    [SerializeField]
+    private List<NodeEvent> events; 
 
     private void Start() {
 
--- Editor/AnimationEventsEditor.cs
+++ Editor/AnimationEventsEditor.cs
@@ -1,7 +1,30 @@
-﻿using UnityEngine;
-using UnityEditor;
+﻿using UnityEditor;
+using ATP.SimplePathAnimator.ReorderableList;
 
+namespace ATP.SimplePathAnimator {
 
-[CustomEditor(typeof (AnimationEvents))]
-public class AnimationEventsEditor : Editor {
+    [CustomEditor(typeof (AnimationEvents))]
+    public class AnimationEventsEditor : Editor {
+
+        private AnimationEvents Script { get; set; }
+
+        private SerializedProperty events;
+
+        private void OnEnable() {
+            Script = (AnimationEvents) target;
+
+            events = serializedObject.FindProperty("events");
+        }
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            ReorderableListGUI.Title("Events");
+            ReorderableListGUI.ListField(events);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+    }
+
 }

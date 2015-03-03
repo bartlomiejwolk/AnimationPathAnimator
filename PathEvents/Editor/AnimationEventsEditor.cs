5ebfd8c0fc3fe9da69d4a6877d7773c1390fcab1
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-03 19:08:46 +0100

    Reorganize members

--- PathEvents/Editor/AnimationEventsEditor.cs
+++ PathEvents/Editor/AnimationEventsEditor.cs
@@ -8,22 +8,37 @@ namespace ATP.SimplePathAnimator.PathEvents {
     [CustomEditor(typeof (AnimationEvents))]
     public class AnimationEventsEditor : Editor {
 
-        private AnimationEvents Script { get; set; }
+        #region FIELDS
+        private SerializedProperty animator;
+
+        private SerializedProperty drawMethodNames;
 
         private SerializedProperty nodeEvents;
-        private SerializedProperty animator;
-        private SerializedProperty drawMethodNames;
+
         private SerializedProperty skin;
+        #endregion
 
-        private void OnEnable() {
-            Script = (AnimationEvents) target;
-
-            nodeEvents = serializedObject.FindProperty("nodeEvents");
-            animator = serializedObject.FindProperty("animator");
-            drawMethodNames = serializedObject.FindProperty("drawMethodNames");
-            skin = serializedObject.FindProperty("skin");
+        #region PROPERTIES
+        public float DefaultNodeLabelHeight {
+            get { return 30; }
         }
 
+        public float DefaultNodeLabelWidth {
+            get { return 100; }
+        }
+
+        public int MethodNameLabelOffsetX {
+            get { return 30; }
+        }
+
+        public int MethodNameLabelOffsetY {
+            get { return -20; }
+        }
+
+        private AnimationEvents Script { get; set; }
+#endregion
+
+        #region UNITY MESSAGES
         public override void OnInspectorGUI() {
             serializedObject.Update();
 
@@ -36,57 +51,27 @@ namespace ATP.SimplePathAnimator.PathEvents {
             serializedObject.ApplyModifiedProperties();
         }
 
+        private void OnEnable() {
+            Script = (AnimationEvents)target;
+
+            nodeEvents = serializedObject.FindProperty("nodeEvents");
+            animator = serializedObject.FindProperty("animator");
+            drawMethodNames = serializedObject.FindProperty("drawMethodNames");
+            skin = serializedObject.FindProperty("skin");
+        }
+
         private void OnSceneGUI() {
             // TODO Guard against null Skin.
             HandleDrawingMethodNames();
         }
-
-        private void HandleDrawingMethodNames() {
-            if (!drawMethodNames.boolValue) return;
-
-            var nodePositions = Script.GetNodePositions();
-            var methodNames = Script.GetMethodNames();
-            var style = Script.Skin.GetStyle("MethodNameLabel");
-
-            DrawNodeLabels(
-                nodePositions,
-                methodNames,
-                MethodNameLabelOffsetX,
-                MethodNameLabelOffsetY,
-                style);
-        }
-
-        public int MethodNameLabelOffsetY {
-            get { return -20; }
-        }
-
-        public int MethodNameLabelOffsetX {
-            get { return 30; }
-        }
-
-        public void DrawNodeLabels(
-            Vector3[] nodePositions,
-            List<string> textValues,
-            int offsetX,
-            int offsetY,
-            GUIStyle style) {
-
-            for (int i = 0; i < textValues.Count; i++) {
-                DrawNodeLabel(
-                    nodePositions[i],
-                    textValues[i],
-                    offsetX,
-                    offsetY,
-                    style);
-            }
-        }
-
-        public void DrawNodeLabel(
-            Vector3 nodePosition,
-            string value,
-            int offsetX,
-            int offsetY,
-            GUIStyle style) {
+        #endregion
+        #region METHODS
+        private void DrawNodeLabel(
+                    Vector3 nodePosition,
+                    string value,
+                    int offsetX,
+                    int offsetY,
+                    GUIStyle style) {
 
             // Translate node's 3d position into screen coordinates.
             var guiPoint = HandleUtility.WorldToGUIPoint(nodePosition);
@@ -109,14 +94,38 @@ namespace ATP.SimplePathAnimator.PathEvents {
             Handles.EndGUI();
         }
 
-        public float DefaultNodeLabelHeight {
-            get { return 30; }
+        private void DrawNodeLabels(
+                    Vector3[] nodePositions,
+                    List<string> textValues,
+                    int offsetX,
+                    int offsetY,
+                    GUIStyle style) {
+
+            for (int i = 0; i < textValues.Count; i++) {
+                DrawNodeLabel(
+                    nodePositions[i],
+                    textValues[i],
+                    offsetX,
+                    offsetY,
+                    style);
+            }
+        }
+        private void HandleDrawingMethodNames() {
+            if (!drawMethodNames.boolValue) return;
+
+            var nodePositions = Script.GetNodePositions();
+            var methodNames = Script.GetMethodNames();
+            var style = Script.Skin.GetStyle("MethodNameLabel");
+
+            DrawNodeLabels(
+                nodePositions,
+                methodNames,
+                MethodNameLabelOffsetX,
+                MethodNameLabelOffsetY,
+                style);
         }
 
-        public float DefaultNodeLabelWidth {
-            get { return 100; }
-        }
-
+        #endregion
     }
 
 }

5c1bab0a9f751af5f4f98f3552b89d5a805a2d68
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-03 09:17:53 +0100

    Create AnimationEvents class

---
+++ AnimationEvents.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationEvents : MonoBehaviour {
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+
+    }
+}
---
+++ Editor/AnimationEventsEditor.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof (AnimationEvents))]
+public class AnimationEventsEditor : Editor {
+}

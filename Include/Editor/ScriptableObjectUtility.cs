aefd823edc6938a4640e39577af3ac43f6542932
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-04 10:38:14 +0100

    Refactor ScriptableObjectUtility class

--- Include/Editor/ScriptableObjectUtility.cs
+++ Include/Editor/ScriptableObjectUtility.cs
@@ -1,30 +1,55 @@
-﻿using System;
-using System.CodeDom;
-using System.IO;
+﻿using System.IO;
 using UnityEditor;
 using UnityEngine;
 
-namespace ATP.SimplePathAnimator.Animator {
+public static class ScriptableObjectUtility {
 
-	public static class ScriptableObjectUtility
-	{
-		/// <summary>
-		//	This makes it easy to create, name and place unique new ScriptableObject asset files.
-		/// </summary>
-		public static T CreateAsset<T> (string path) where T : ScriptableObject
-		{
-            // Path cannot be empty.
-		    if (path == "") return null;
+    public static void CreateAsset<T>() where T : ScriptableObject {
+        var asset = ScriptableObject.CreateInstance<T>();
 
-			T asset = ScriptableObject.CreateInstance<T> ();
-			
-			AssetDatabase.CreateAsset (asset, path);
-			AssetDatabase.SaveAssets ();
-			AssetDatabase.Refresh();
-            //EditorUtility.FocusProjectWindow ();
-			Selection.activeObject = asset;
+        var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (path == "") {
+            path = "Assets";
+        }
+        else if (Path.GetExtension(path) != "") {
+            path =
+                path.Replace(
+                    Path.GetFileName(
+                        AssetDatabase.GetAssetPath(Selection.activeObject)),
+                    "");
+        }
 
-		    return asset;
-		}
-	}
+        var assetPathAndName =
+            AssetDatabase.GenerateUniqueAssetPath(
+                path + "/New " + typeof (T) + ".asset");
+
+        AssetDatabase.CreateAsset(asset, assetPathAndName);
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+    }
+
+    //	This makes it easy to create, name and place unique new ScriptableObject asset files.
+    /// <summary>
+    /// </summary>
+    public static T CreateAsset<T>(string path) where T : ScriptableObject {
+        // Path cannot be empty.
+        if (path == "") {
+            Debug.LogWarning("Path to create new asset cannot be empty.");
+
+            return null;
+        }
+
+        var asset = ScriptableObject.CreateInstance<T>();
+
+        AssetDatabase.CreateAsset(asset, path);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        Selection.activeObject = asset;
+
+        return asset;
+    }
+
 }

a5e2df989165f33c98117165f89725b0238067c4
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-04 12:09:41 +0100

    Create regions

--- Animator/AnimatorSettings.cs
+++ Animator/AnimatorSettings.cs
@@ -5,21 +5,9 @@ namespace ATP.SimplePathAnimator.Animator {
 
     public class AnimatorSettings : ScriptableObject {
 
-        [SerializeField]
-        private KeyCode playPauseKey = KeyCode.Space;
-
-        public KeyCode PlayPauseKey {
-            get { return playPauseKey; }
-            set { playPauseKey = value; }
-        }
-
+        #region SHORTCUT FIELDS
         [SerializeField]
         private KeyCode easeModeKey = KeyCode.U;
-
-        // TODO Add setters everywhere.
-        public KeyCode EaseModeKey {
-            get { return easeModeKey; }
-        }
 
         /// <summary>
         ///     Key shortcut to jump to the end of the animation.
@@ -27,67 +15,97 @@ namespace ATP.SimplePathAnimator.Animator {
         [SerializeField]
         private KeyCode jumpToEndKey = KeyCode.L;
 
+        [SerializeField]
+        private KeyCode jumpToNextNodeKey = KeyCode.L;
+
+        [SerializeField]
+        private KeyCode jumpToPreviousNodeKey = KeyCode.H;
+
+        [SerializeField]
+        private KeyCode jumpToStartKey = KeyCode.H;
+
+        [SerializeField]
+        private KeyCode longJumpBackwardKey = KeyCode.J;
+
+        [SerializeField]
+        private KeyCode longJumpForwardKey = KeyCode.K;
+
+        [SerializeField]
+        private float longJumpValue = 0.01f;
+
+        [SerializeField]
+        private KeyCode modKey = KeyCode.RightAlt;
+
+        [SerializeField]
+        private Color moveAllModeColor = Color.red;
+
+        [SerializeField]
+        private KeyCode moveAllModeKey = KeyCode.P;
+
+        [SerializeField]
+        private KeyCode noneModeKey = KeyCode.Y;
+
+        [SerializeField]
+        private KeyCode playPauseKey = KeyCode.Space;
+
+        [SerializeField]
+        private KeyCode rotationModeKey = KeyCode.I;
+
+        [SerializeField]
+        private KeyCode shortJumpBackwardKey = KeyCode.J;
+
+        [SerializeField]
+        private KeyCode shortJumpForwardKey = KeyCode.K;
+
+        [SerializeField]
+        private KeyCode tiltingModeKey = KeyCode.O;
+
+        [SerializeField]
+        private KeyCode updateAllKey = KeyCode.G;
+#endregion
+        #region SHORTCUT PROPERTIES
+
+        // TODO Add setters everywhere.
+        public KeyCode EaseModeKey {
+            get { return easeModeKey; }
+        }
+
         public KeyCode JumpToEndKey {
             get { return jumpToEndKey; }
         }
-
-        [SerializeField]
-        private KeyCode jumpToNextNodeKey = KeyCode.L;
 
         public KeyCode JumpToNextNodeKey {
             get { return jumpToNextNodeKey; }
         }
 
-        [SerializeField]
-        private KeyCode jumpToPreviousNodeKey = KeyCode.H;
-
         public KeyCode JumpToPreviousNodeKey {
             get { return jumpToPreviousNodeKey; }
         }
-
-        [SerializeField]
-        private KeyCode jumpToStartKey = KeyCode.H;
 
         public KeyCode JumpToStartKey {
             get { return jumpToStartKey; }
         }
 
-        [SerializeField]
-        private KeyCode longJumpBackwardKey = KeyCode.J;
-
         public KeyCode LongJumpBackwardKey {
             get { return longJumpBackwardKey; }
         }
-
-        [SerializeField]
-        private KeyCode longJumpForwardKey = KeyCode.K;
 
         public KeyCode LongJumpForwardKey {
             get { return longJumpForwardKey; }
         }
 
-        [SerializeField]
-        private float longJumpValue = 0.01f;
-
         public float LongJumpValue {
             get { return longJumpValue; }
         }
-
-        [SerializeField]
-        private KeyCode modKey = KeyCode.RightAlt;
 
         public virtual KeyCode ModKey {
             get { return modKey; }
         }
 
-        [SerializeField]
-        private Color moveAllModeColor = Color.red;
         public virtual Color MoveAllModeColor {
             get { return moveAllModeColor; }
         }
 
-        [SerializeField]
-        private KeyCode moveAllModeKey = KeyCode.P;
         public virtual KeyCode MoveAllModeKey {
             get { return moveAllModeKey; }
         }
@@ -95,47 +113,33 @@ namespace ATP.SimplePathAnimator.Animator {
         //public virtual KeyCode MoveSingleModeKey {
         //    get { return KeyCode.Y; }
         //}
-
-        [SerializeField]
-        private KeyCode noneModeKey = KeyCode.Y;
         public virtual KeyCode NoneModeKey {
             get { return noneModeKey; }
         }
 
+        public KeyCode PlayPauseKey {
+            get { return playPauseKey; }
+            set { playPauseKey = value; }
+        }
         //public virtual KeyCode PlayPauseKey {
         //    get { return KeyCode.Space; }
         //}
-
-        [SerializeField]
-        private KeyCode rotationModeKey = KeyCode.I;
         public virtual KeyCode RotationModeKey {
             get { return rotationModeKey; }
         }
-
-        [SerializeField]
-        private KeyCode shortJumpBackwardKey = KeyCode.J;
         public KeyCode ShortJumpBackwardKey {
             get { return shortJumpBackwardKey; }
         }
-
-        [SerializeField]
-        private KeyCode shortJumpForwardKey = KeyCode.K;
         public KeyCode ShortJumpForwardKey {
             get { return shortJumpForwardKey; }
         }
-
-        [SerializeField]
-        private KeyCode tiltingModeKey = KeyCode.O;
         public virtual KeyCode TiltingModeKey {
             get { return tiltingModeKey; }
         }
-
-        [SerializeField]
-        private KeyCode updateAllKey = KeyCode.G;
         public virtual KeyCode UpdateAllKey {
             get { return updateAllKey; }
         }
-
+        #endregion
     }
 
 }

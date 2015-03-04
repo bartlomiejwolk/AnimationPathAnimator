85942919b4b3db9baef1d4cbf534e1fe66ebc188
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-04 10:06:23 +0100

    Create Settings class

---
+++ Animator/Settings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Settings : ScriptableObject{
+
+    private KeyCode playPauseKey = KeyCode.Space;
+
+    public KeyCode PlayPauseKey {
+        get { return playPauseKey; }
+        set { playPauseKey = value; }
+    }
+
+}

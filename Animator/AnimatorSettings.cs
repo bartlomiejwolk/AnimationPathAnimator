ad38321ccdaa2d15637cb078dba462a705ff5459
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-04 10:36:46 +0100

    Create AnimatorSettings class

---
+++ Animator/AnimatorSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ATP.SimplePathAnimator.Animator {
+
+    public class AnimatorSettings : ScriptableObject {
+
+        [SerializeField]
+        private KeyCode playPauseKey = KeyCode.Space;
+
+        public KeyCode PlayPauseKey {
+            get { return playPauseKey; }
+            set { playPauseKey = value; }
+        }
+
+    }
+
+}

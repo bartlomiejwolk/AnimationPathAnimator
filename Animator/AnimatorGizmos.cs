5f1a0e789664e6316cd53d0eec26582b0511aed4
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-07 09:26:42 +0100

    Refactor CopyIconsToGizmosFolder()

--- Animator/AnimatorGizmos.cs
+++ Animator/AnimatorGizmos.cs
@@ -22,52 +22,6 @@ namespace ATP.SimplePathAnimator.Animator {
 
         public AnimatorGizmos(PathAnimatorSettings settings) {
             this.settings = settings;
-
-            // TODO Use directive to disable this code in standalone.
-            CopyIconsToGizmosFolder();
-        }
-
-        private void CopyIconsToGizmosFolder() {
-
-            // Create Asset/Gizmos folder if not exists.
-            if (!Directory.Exists(Application.dataPath + "/Gizmos")) {
-                Directory.CreateDirectory(Application.dataPath + "/Gizmos");
-            }
-
-            // Check if settings asset has any paths to be searched for icons.
-            if (Settings.IconsSourceDirs == null) return;
-
-            // For each path check if specified folder exists..
-            foreach (var iconDir in Settings.IconsSourceDirs) {
-                if (Directory.Exists(Application.dataPath + iconDir)) {
-                    // Copy icon to Asset/Gizmos folder.
-                    CopyIcon(iconDir, Settings.CurrentRotationPointGizmoIcon);
-                    CopyIcon(iconDir, Settings.ForwardPointIcon);
-                    CopyIcon(iconDir, Settings.RotationPointGizmoIcon);
-                    CopyIcon(iconDir, Settings.TargetGizmoIcon);
-
-                    break;
-                }
-            }
-        }
-
-        private void CopyIcon(string sourceDir, string iconName) {
-            // Check if icon file exists in Assets/Gizmos folder.
-            if (!File.Exists(
-                Application.dataPath + "/Gizmos/"
-                + iconName + ".png")) {
-
-                // TODO If source directory doesn't exist, show info about how
-                // to copy icons to the Gizmos folder.
-
-                // Copy icon.
-                FileUtil.CopyFileOrDirectory(
-                    Application.dataPath
-                        + sourceDir
-                        + iconName + ".png",
-                    Application.dataPath + "/Gizmos/"
-                        + iconName + ".png");
-            }
         }
 
         public void DrawAnimationCurve(PathData pathData,

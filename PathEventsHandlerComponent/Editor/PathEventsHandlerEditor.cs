9e9d3755625f84001c74bde34f733930702e7840
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-10 11:34:26 +0100

    Draw advanced settings foldout

--- PathEventsHandlerComponent/Editor/PathEventsHandlerEditor.cs
+++ PathEventsHandlerComponent/Editor/PathEventsHandlerEditor.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEditor;
+using UnityEngine;
 
 namespace ATP.SimplePathAnimator.PathEventsHandlerComponent {
 
@@ -7,8 +8,65 @@ namespace ATP.SimplePathAnimator.PathEventsHandlerComponent {
 
         private PathEventsHandler Script { get; set; }
 
+        private SerializedProperty pathAnimator;
+        private SerializedProperty advancedSettingsFoldout;
+        private SerializedProperty skin;
+        private SerializedProperty settings;
+
         private void OnEnable() {
             Script = target as PathEventsHandler;
+
+            pathAnimator =
+                serializedObject.FindProperty("pathAnimator");
+            advancedSettingsFoldout =
+                serializedObject.FindProperty("advancedSettingsFoldout");
+            skin =
+                serializedObject.FindProperty("skin");
+            settings =
+                serializedObject.FindProperty("settings");
+        }
+
+        public override void OnInspectorGUI() {
+            // TODO Extract method.
+            EditorGUILayout.PropertyField(
+                pathAnimator,
+                new GUIContent(
+                    "Animator",
+                    ""));
+
+            DrawAdvancedSettingsFoldout();
+            DrawAdvancedSettingsControls();
+        }
+
+        private void DrawAdvancedSettingsFoldout() {
+            serializedObject.Update();
+
+            advancedSettingsFoldout.boolValue = EditorGUILayout.Foldout(
+                advancedSettingsFoldout.boolValue,
+                new GUIContent(
+                    "Advanced Settings",
+                    ""));
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void DrawAdvancedSettingsControls() {
+            if (advancedSettingsFoldout.boolValue) {
+                DrawSettingsAssetField();
+                EditorGUILayout.PropertyField(skin);
+            }
+        }
+
+        private void DrawSettingsAssetField() {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(
+                settings,
+                new GUIContent(
+                    "Settings Asset",
+                    ""));
+
+            serializedObject.ApplyModifiedProperties();
         }
 
     }

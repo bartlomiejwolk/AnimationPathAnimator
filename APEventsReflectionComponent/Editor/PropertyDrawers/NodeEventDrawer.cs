574eea435c05d42b40c9df619d5de114d76b402d
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-11 10:02:00 +0100

    Return if false

--- APEventsReflectionComponent/Editor/PropertyDrawers/NodeEventDrawer.cs
+++ APEventsReflectionComponent/Editor/PropertyDrawers/NodeEventDrawer.cs
@@ -17,6 +17,7 @@ namespace ATP.AnimationPathAnimator.APEventsReflectionComponent {
         // Margin between properties.
         private const int PropMargin = 4;
 
+        // Space between rows.
         private const int RowsSpace = 8;
 
         // Overall hight of the serialized property.
@@ -26,7 +27,7 @@ namespace ATP.AnimationPathAnimator.APEventsReflectionComponent {
 
             return base.GetPropertyHeight(property, label)
                 * Rows // Each row is 16 px high.
-                + (Rows - 1) * RowsSpace; // Add 4 px for each spece between rows.
+                + (Rows - 1) * RowsSpace;
         }
 
         public override void OnGUI(
@@ -62,65 +63,63 @@ namespace ATP.AnimationPathAnimator.APEventsReflectionComponent {
                     sourceGO,
                     new GUIContent("Source GO", ""));
 
-            // TODO Use return instead.
             // If source GO is assigned..
-            if (sourceGO.objectReferenceValue != null) {
-                // Get reference to source GO.
-                var sourceGORef = sourceGO.objectReferenceValue as GameObject;
-                // Get source game object components.
-                var sourceComponents = sourceGORef.GetComponents<Component>();
-                // Initialize array for source GO component names.
-                var sourceCoNames= new string[sourceComponents.Length];
-                // Fill array with component names.
-                for (int i = 0; i < sourceCoNames.Length; i++) {
-                    sourceCoNames[i] = sourceComponents[i].GetType().ToString();
-                }
-                // Make sure that current name index corresponds to a component.
-                // Important when changing source game object.
-                if (sourceComponentIndex.intValue > sourceCoNames.Length - 1) {
-                    sourceComponentIndex.intValue = 0;
-                }
-
-                // Display dropdown game object component list.
-                sourceComponentIndex.intValue = EditorGUI.Popup(
-                     new Rect(
-                        pos.x,
-                        pos.y + 1 * (PropHeight + PropMargin),
-                        pos.width,
-                        PropHeight), 
-                    "Source Component",
-                    sourceComponentIndex.intValue,
-                    sourceCoNames);
-
-                // Update source component ref. in the NodeEvent property.
-                sourceCo.objectReferenceValue =
-                    sourceComponents[sourceComponentIndex.intValue];
-
-                // Get target component method names.
-                var methods = sourceComponents[sourceComponentIndex.intValue]
-                    .GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
-                // Initialize array with method names.
-                var methodNames = new string[methods.Length];
-                // Fill array with method names.
-                for (int i = 0; i < methodNames.Length; i++) {
-                    methodNames[i] = methods[i].Name;
-                }
-
-                // Display dropdown with component properties.
-                sourceMethodIndex.intValue = EditorGUI.Popup(
-                    new Rect(
-                        pos.x,
-                        pos.y + 2 * (PropHeight + PropMargin),
-                        pos.width,
-                        PropHeight),
-                    "Methods",
-                    sourceMethodIndex.intValue,
-                    methodNames);
-
-                // Update method name in the NodeEvent property.
-                sourceMethodName.stringValue =
-                    methodNames[sourceMethodIndex.intValue];
+            if (sourceGO.objectReferenceValue == null) return;
+            // Get reference to source GO.
+            var sourceGORef = sourceGO.objectReferenceValue as GameObject;
+            // Get source game object components.
+            var sourceComponents = sourceGORef.GetComponents<Component>();
+            // Initialize array for source GO component names.
+            var sourceCoNames= new string[sourceComponents.Length];
+            // Fill array with component names.
+            for (int i = 0; i < sourceCoNames.Length; i++) {
+                sourceCoNames[i] = sourceComponents[i].GetType().ToString();
             }
+            // Make sure that current name index corresponds to a component.
+            // Important when changing source game object.
+            if (sourceComponentIndex.intValue > sourceCoNames.Length - 1) {
+                sourceComponentIndex.intValue = 0;
+            }
+
+            // Display dropdown game object component list.
+            sourceComponentIndex.intValue = EditorGUI.Popup(
+                 new Rect(
+                    pos.x,
+                    pos.y + 1 * (PropHeight + PropMargin),
+                    pos.width,
+                    PropHeight), 
+                "Source Component",
+                sourceComponentIndex.intValue,
+                sourceCoNames);
+
+            // Update source component ref. in the NodeEvent property.
+            sourceCo.objectReferenceValue =
+                sourceComponents[sourceComponentIndex.intValue];
+
+            // Get target component method names.
+            var methods = sourceComponents[sourceComponentIndex.intValue]
+                .GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+            // Initialize array with method names.
+            var methodNames = new string[methods.Length];
+            // Fill array with method names.
+            for (int i = 0; i < methodNames.Length; i++) {
+                methodNames[i] = methods[i].Name;
+            }
+
+            // Display dropdown with component properties.
+            sourceMethodIndex.intValue = EditorGUI.Popup(
+                new Rect(
+                    pos.x,
+                    pos.y + 2 * (PropHeight + PropMargin),
+                    pos.width,
+                    PropHeight),
+                "Methods",
+                sourceMethodIndex.intValue,
+                methodNames);
+
+            // Update method name in the NodeEvent property.
+            sourceMethodName.stringValue =
+                methodNames[sourceMethodIndex.intValue];
 
             // Don't draw parameter field if source GO is not specified.
             if (sourceGO.objectReferenceValue == null) return;

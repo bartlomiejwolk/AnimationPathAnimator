8b558bca16e24c446e539ac3721be79fe029c4e0
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-13 21:17:58 +0100

    Add public property

--- APAnimatorComponent/Editor/SceneTool.cs
+++ APAnimatorComponent/Editor/SceneTool.cs
@@ -4,14 +4,19 @@ namespace ATP.AnimationPathAnimator.APAnimatorComponent {
 
     public static class SceneTool {
 
-        public static Tool LastTool = Tool.None;
+        private static Tool lastTool = Tool.None;
+
+        public static Tool LastTool {
+            get { return lastTool; }
+            set { lastTool = value; }
+        }
 
         public static void RememberCurrentTool() {
             // Remember active scene tool.
-            if (Tools.current != Tool.None) {
-                LastTool = Tools.current;
-                Tools.current = Tool.None;
-            }
+            if (Tools.current == Tool.None) return;
+
+            LastTool = Tools.current;
+            Tools.current = Tool.None;
         }
 
         public static void RestoreTool() {

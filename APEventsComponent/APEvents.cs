631dddca221d6d831046f40750799094d4978987
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-13 21:35:22 +0100

    Cleanup class

--- APEventsComponent/APEvents.cs
+++ APEventsComponent/APEvents.cs
@@ -4,73 +4,13 @@ using UnityEngine;
 
 namespace ATP.AnimationPathAnimator.APEventsComponent {
 
-    [RequireComponent(typeof(APAnimator))]
+    [RequireComponent(typeof (APAnimator))]
     public class APEvents : MonoBehaviour {
-
-        #region FIELDS
-        [SerializeField]
-        private GUISkin skin;
-
-        [SerializeField]
-        private APAnimator animator;
-
-        [SerializeField]
-        private APEventsSettings settings;
-
-        [SerializeField]
-        private bool advancedSettingsFoldout;
-
-        [SerializeField]
-        private List<NodeEventSlot> nodeEventSlots;
-
-        [SerializeField]
-        private bool drawMethodNames = true;
-        #endregion
-
-        #region PROPERTIES
-        public APAnimator Animator {
-            get { return animator; }
-            set { animator = value; }
-        }
-
-        public GUISkin Skin {
-            get { return skin; }
-            set { skin = value; }
-        }
-
-        public APEventsSettings Settings {
-            get { return settings; }
-        }
-
-        private List<NodeEventSlot> NodeEventSlots {
-            get { return nodeEventSlots; }
-        }
-
-        #endregion
-
-        #region UNITY MESSAGES
-
-        private void OnDisable() {
-            Animator.NodeReached -= Animator_NodeReached;
-        }
-
-        private void OnEnable() {
-            if (Animator == null) return; 
-
-            Animator.NodeReached += Animator_NodeReached;
-        }
-
-        private void Reset() {
-            animator = GetComponent<APAnimator>();
-            settings =
-                Resources.Load<APEventsSettings>("DefaultAPEventsSettings");
-            skin = Resources.Load("DefaultAPEventsSkin") as GUISkin;
-        }
-        #endregion
         #region EVENT HANDLERS
+
         private void Animator_NodeReached(
-                    object sender,
-                    NodeReachedEventArgs arg) {
+            object sender,
+            NodeReachedEventArgs arg) {
 
             // Return if there's no event slot created for current path node.
             if (arg.NodeIndex > NodeEventSlots.Count - 1) return;
@@ -80,7 +20,7 @@ namespace ATP.AnimationPathAnimator.APEventsComponent {
             if (nodeEvent.SourceGO == null) return;
             // Get method metadata.
             var methodInfo = nodeEvent.SourceCo.GetType()
-                    .GetMethod(nodeEvent.SourceMethodName);
+                .GetMethod(nodeEvent.SourceMethodName);
             // Return if method info couldn't be loaded.
             if (methodInfo == null) return;
             // Get method parameters.
@@ -103,24 +43,76 @@ namespace ATP.AnimationPathAnimator.APEventsComponent {
         }
 
         #endregion
+
+        #region FIELDS
+
+        [SerializeField]
+        private bool advancedSettingsFoldout;
+
+        [SerializeField]
+        private APAnimator animator;
+
+        [SerializeField]
+        private bool drawMethodNames = true;
+
+        [SerializeField]
+        private List<NodeEventSlot> nodeEventSlots;
+
+        [SerializeField]
+        private APEventsSettings settings;
+
+        [SerializeField]
+        private GUISkin skin;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public APAnimator Animator {
+            get { return animator; }
+            set { animator = value; }
+        }
+
+        public APEventsSettings Settings {
+            get { return settings; }
+        }
+
+        public GUISkin Skin {
+            get { return skin; }
+            set { skin = value; }
+        }
+
+        private List<NodeEventSlot> NodeEventSlots {
+            get { return nodeEventSlots; }
+        }
+
+        #endregion
+
+        #region UNITY MESSAGES
+
+        private void OnDisable() {
+            Animator.NodeReached -= Animator_NodeReached;
+        }
+
+        private void OnEnable() {
+            if (Animator == null) return;
+
+            Animator.NodeReached += Animator_NodeReached;
+        }
+
+        private void Reset() {
+            animator = GetComponent<APAnimator>();
+            settings =
+                Resources.Load<APEventsSettings>("DefaultAPEventsSettings");
+            skin = Resources.Load("DefaultAPEventsSkin") as GUISkin;
+        }
+
+        #endregion
+
         #region METHODS
 
-        private Vector3[] GetNodePositions(int nodesNo) {
-            var nodePositions =
-                Animator.GetGlobalNodePositions(nodesNo);
-
-            return nodePositions;
-        }
-        private string[] GetMethodNames() {
-            var methodNames = new string[NodeEventSlots.Count];
-
-            for (int i = 0; i < NodeEventSlots.Count; i++) {
-                methodNames[i] = NodeEventSlots[i].SourceMethodName;
-            }
-
-            return methodNames;
-        }
-
+        // TODO Rename to RequiredAssetsLoaded().
+        // TODO Update Editor class.
         private bool AssetsLoaded() {
             if (Settings != null
                 && Skin != null) {
@@ -129,8 +121,24 @@ namespace ATP.AnimationPathAnimator.APEventsComponent {
             return false;
         }
 
-        #endregion   
+        private string[] GetMethodNames() {
+            var methodNames = new string[NodeEventSlots.Count];
 
+            for (var i = 0; i < NodeEventSlots.Count; i++) {
+                methodNames[i] = NodeEventSlots[i].SourceMethodName;
+            }
+
+            return methodNames;
+        }
+
+        private Vector3[] GetNodePositions(int nodesNo) {
+            var nodePositions =
+                Animator.GetGlobalNodePositions(nodesNo);
+
+            return nodePositions;
+        }
+
+        #endregion
     }
 
 }

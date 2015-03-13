817db6a49268c73d590e4520396a25e89ab52e5d
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-14 00:11:31 +0100

    Reorder

--- APEventsComponent/APEvents.cs
+++ APEventsComponent/APEvents.cs
@@ -6,44 +6,6 @@ namespace ATP.AnimationPathAnimator.APEventsComponent {
 
     [RequireComponent(typeof (APAnimator))]
     public class APEvents : MonoBehaviour {
-        #region EVENT HANDLERS
-
-        private void Animator_NodeReached(
-            object sender,
-            NodeReachedEventArgs arg) {
-
-            // Return if there's no event slot created for current path node.
-            if (arg.NodeIndex > NodeEventSlots.Count - 1) return;
-            // Get event slot.
-            var nodeEvent = NodeEventSlots[arg.NodeIndex];
-            // Return if source GO was not specified in the event slot.
-            if (nodeEvent.SourceGO == null) return;
-            // Get method metadata.
-            var methodInfo = nodeEvent.SourceCo.GetType()
-                .GetMethod(nodeEvent.SourceMethodName);
-            // Return if method info couldn't be loaded.
-            if (methodInfo == null) return;
-            // Get method parameters.
-            var methodParams = methodInfo.GetParameters();
-
-            // Method has no parameters.
-            if (methodParams.Length == 0) {
-                // Invoke method.
-                methodInfo.Invoke(nodeEvent.SourceCo, null);
-            }
-            // Method has one parameter.
-            else if (methodParams.Length == 1) {
-                // Return if the parameter is not a string.
-                if (methodParams[0].ParameterType.Name != "String") return;
-                // Create string parameter argument.
-                var stringParam = new object[] {nodeEvent.MethodArg};
-                // Invoke method with string parameter.
-                methodInfo.Invoke(nodeEvent.SourceCo, stringParam);
-            }
-        }
-
-        #endregion
-
         #region FIELDS
 
         [SerializeField]
@@ -105,6 +67,44 @@ namespace ATP.AnimationPathAnimator.APEventsComponent {
             settings =
                 Resources.Load<APEventsSettings>("DefaultAPEventsSettings");
             skin = Resources.Load("DefaultAPEventsSkin") as GUISkin;
+        }
+
+        #endregion
+
+        #region EVENT HANDLERS
+
+        private void Animator_NodeReached(
+            object sender,
+            NodeReachedEventArgs arg) {
+
+            // Return if there's no event slot created for current path node.
+            if (arg.NodeIndex > NodeEventSlots.Count - 1) return;
+            // Get event slot.
+            var nodeEvent = NodeEventSlots[arg.NodeIndex];
+            // Return if source GO was not specified in the event slot.
+            if (nodeEvent.SourceGO == null) return;
+            // Get method metadata.
+            var methodInfo = nodeEvent.SourceCo.GetType()
+                .GetMethod(nodeEvent.SourceMethodName);
+            // Return if method info couldn't be loaded.
+            if (methodInfo == null) return;
+            // Get method parameters.
+            var methodParams = methodInfo.GetParameters();
+
+            // Method has no parameters.
+            if (methodParams.Length == 0) {
+                // Invoke method.
+                methodInfo.Invoke(nodeEvent.SourceCo, null);
+            }
+            // Method has one parameter.
+            else if (methodParams.Length == 1) {
+                // Return if the parameter is not a string.
+                if (methodParams[0].ParameterType.Name != "String") return;
+                // Create string parameter argument.
+                var stringParam = new object[] { nodeEvent.MethodArg };
+                // Invoke method with string parameter.
+                methodInfo.Invoke(nodeEvent.SourceCo, stringParam);
+            }
         }
 
         #endregion

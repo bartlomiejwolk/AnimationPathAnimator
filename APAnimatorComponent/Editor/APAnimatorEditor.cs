6f11be41b9ffab4b7c403afdb1d8ad08409405f6
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-20 11:49:47 +0100

    Extract method

--- APAnimatorComponent/Editor/APAnimatorEditor.cs
+++ APAnimatorComponent/Editor/APAnimatorEditor.cs
@@ -103,7 +103,7 @@ namespace ATP.AnimationPathAnimator.APAnimatorComponent {
             // Update animated GO.
             Utilities.InvokeMethodWithReflection(
                 Script,
-                "UpdateAnimation",
+                "HandleUpdateAnimGOInSceneView",
                 null);
         }
 
@@ -988,30 +988,36 @@ namespace ATP.AnimationPathAnimator.APAnimatorComponent {
             Undo.RecordObject(Script.PathData, "Tilting curve changed.");
 
             if (Script.SettingsAsset.UpdateAllMode) {
-                // Get old ease value.
-                var oldValue = Script.PathData.GetTiltingValueAtIndex(keyIndex);
-                // Guard against null division.
-                if (Utilities.FloatsEqual(
-                    oldValue,
-                    0,
-                    GlobalConstants.FloatPrecision)) return;
-                // Calculate multiplier.
-                var multiplier = newValue / oldValue;
-
-                // Don't let ease value reach zero.
-                if (Utilities.FloatsEqual(
-                    multiplier,
-                    0,
-                    GlobalConstants.FloatPrecision)) return;
-
-                // Multiply each single ease value.
-                Script.PathData.MultiplyTiltingCurveValues(multiplier);
+                MultiplyTiltingValues(
+                    Script.PathData.GetTiltingValueAtIndex(keyIndex),
+                    newValue);
             }
             else {
                 Script.PathData.UpdateTiltingValue(keyIndex, newValue);
             }
 
             EditorUtility.SetDirty(Script.PathData);
+        }
+
+        // todo docs
+        private void MultiplyTiltingValues(float oldValue, float newValue) {
+            // Guard against null division.
+            if (Utilities.FloatsEqual(
+                oldValue,
+                0,
+                GlobalConstants.FloatPrecision)) return;
+
+            // Calculate multiplier.
+            var multiplier = newValue / oldValue;
+
+            // Don't let ease value reach zero.
+            if (Utilities.FloatsEqual(
+                multiplier,
+                0,
+                GlobalConstants.FloatPrecision)) return;
+
+            // Multiply each single ease value.
+            Script.PathData.MultiplyTiltingCurveValues(multiplier);
         }
 
         #endregion CALLBACK HANDLERS

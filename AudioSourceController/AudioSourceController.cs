334ec6cedd3daf34b69b99230ab2d42e8420bd73
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-23 16:16:23 +0100

    Create editor class

--- AudioSourceController/AudioSourceController.cs
+++ AudioSourceController/AudioSourceController.cs
@@ -3,7 +3,15 @@ using System.Collections;
 
 namespace ATP.AnimationPathTools.AudioSourceControllerComponent {
 
-    public class AudioSourceController : MonoBehaviour {
+    /// <summary>
+    /// Allows controlling <c>AudioSource</c> component from inspector
+    /// and with keyboard shortcuts.
+    /// </summary>
+    // todo add menu option to create component
+    public sealed class AudioSourceController : MonoBehaviour {
+
+        [SerializeField]
+        private AudioSource audioSource;
 
         private void Start() {
 
---
+++ AudioSourceController/Editor/AudioSourceControllerEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ATP.AnimationPathTools.AudioSourceControllerComponent {
+
+    [CustomEditor(typeof (AudioSourceController))]
+    public sealed class AudioSourceControllerEditor : Editor {
+
+        private SerializedProperty audioSource;
+
+        private void OnEnable() {
+            audioSource = serializedObject.FindProperty("audioSource");
+        }
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(
+                audioSource,
+                new GUIContent(
+                    "Audio Source",
+                    ""));
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+    }
+
+}

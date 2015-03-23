ba458ef13a388ec8cca7d4637944bee012eb6833
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-23 16:01:42 +0100

    Create audio source controller component

---
+++ AudioSourceController/AudioSourceController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ATP.AnimationPathTools.AudioSourceControllerComponent {
+
+    public class AudioSourceController : MonoBehaviour {
+
+        private void Start() {
+
+        }
+
+        private void Update() {
+
+        }
+
+    }
+
+}

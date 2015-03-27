cd33917aa24492fd28bf7172a34747ad5d43d656
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-27 13:14:16 +0100

    Add entries to Assets menu

--- Editor/ComponentCreator.cs
+++ Editor/ComponentCreator.cs
@@ -6,29 +6,29 @@ namespace ATP.AnimationPathTools {
 
     public static class ComponentCreator {
 
-        //[MenuItem("Assets/Create/ATP/AnimationPathAnimator/Animator SettingsAsset")]
-        //private static void CreateAnimatorSettingsAsset() {
-        //    ScriptableObjectUtility.CreateAsset<AnimatorSettings>("AnimatorSettings");
-        //}
+        [MenuItem("Assets/Create/ATP/AnimationPathTools/Animation Path")]
+        private static void CreatePathAsset() {
+            ScriptableObjectUtility.CreateAsset<PathData>("AnimationPath");
+        }
 
-        //[MenuItem("Assets/Create/ATP/AnimationPathAnimator/Events Reflection SettingsAsset")]
-        //private static void CreateAPEventsReflectionSettingsAsset() {
-        //    ScriptableObjectUtility.CreateAsset<EventsSettings>("EventsSettings");
-        //}
+        [MenuItem("Assets/Create/ATP/AnimationPathTools/Animator Settings")]
+        private static void CreateAnimatorSettingsAsset() {
+            ScriptableObjectUtility.CreateAsset<AnimatorSettings>("AnimatorSettings");
+        }
 
-        //[MenuItem("Assets/Create/ATP/AnimationPathAnimator/Animator Data")]
-        //private static void CreatePathAsset() {
-        //    ScriptableObjectUtility.CreateAsset<PathData>("APAnimatorData");
-        //}
+        [MenuItem("Assets/Create/ATP/AnimationPathTools/Events Settings")]
+        private static void CreateAPEventsReflectionSettingsAsset() {
+            ScriptableObjectUtility.CreateAsset<EventsSettings>("EventsSettings");
+        }
 
-        [MenuItem("Component/ATP/AnimationPathAnimator/Animator")]
+        [MenuItem("Component/ATP/AnimationPathTools/Animator")]
         private static void AddAPAnimatorComponent() {
             if (Selection.activeGameObject != null) {
                 Selection.activeGameObject.AddComponent(typeof(Animator));
             }
         }
 
-        [MenuItem("Component/ATP/AnimationPathAnimator/Events")]
+        [MenuItem("Component/ATP/AnimationPathTools/Events")]
         private static void AddAPEventsComponent() {
             if (Selection.activeGameObject != null) {
                 Selection.activeGameObject.AddComponent(typeof(Events));

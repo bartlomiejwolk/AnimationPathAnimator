efafbf8a0c99bf6eaa46846817550cf0e68a6546
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-03-28 09:23:34 +0100

    Disable tilting lerping

--- AnimatorComponent/Animator.cs
+++ AnimatorComponent/Animator.cs
@@ -694,8 +694,9 @@ namespace ATP.AnimationPathTools.AnimatorComponent {
                 // todo create setting in animator
                 0.01f);
             // Update value on Z axis.
-            //eulerAngles = new Vector3(eulerAngles.x, eulerAngles.y, zRotation);
-            eulerAngles = new Vector3(eulerAngles.x, eulerAngles.y, zRotationLerped);
+            eulerAngles = new Vector3(eulerAngles.x, eulerAngles.y, zRotation);
+            // todo enable tilting lerping
+            //eulerAngles = new Vector3(eulerAngles.x, eulerAngles.y, zRotationLerped);
             // Update animated GO rotation.
             AnimatedGO.rotation = Quaternion.Euler(eulerAngles);
         

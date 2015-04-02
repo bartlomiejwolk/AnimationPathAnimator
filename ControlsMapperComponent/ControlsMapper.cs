ac0d9bf4c8df2ad737653815c182a999f7e351c4
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-04-02 16:12:38 +0200

    Create ControlsMapper class

---
+++ ControlsMapperComponent/ControlsMapper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ATP.AnimationPathTools.ControlsMapperComponent {
+
+    [RequireComponent(typeof(AnimatorComponent.Animator))]
+    public sealed class ControlsMapper : MonoBehaviour {
+
+        [SerializeField]
+        private AnimatorComponent.Animator animator;
+
+        /// <summary>
+        /// Source animator component.
+        /// </summary>
+        public AnimatorComponent.Animator Animator {
+            get { return animator; }
+            set { animator = value; }
+        }
+
+    }
+
+}
---
+++ ControlsMapperComponent/Editor/ControlsMapperEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ATP.AnimationPathTools.ControlsMapperComponent {
+
+    [CustomEditor(typeof (ControlsMapper))]
+    public class ControlsMapperEditor : Editor {
+
+        private ControlsMapper Script;
+
+        private void OnEnable() {
+            Script = (ControlsMapper) target;
+        }
+
+        public override void OnInspectorGUI() {
+            DrawAnimatorField();
+        }
+
+        private void DrawAnimatorField() {
+            Script.Animator = (AnimatorComponent.Animator)EditorGUILayout.ObjectField(
+                new GUIContent(
+                    "Animator",
+                    ""),
+                Script.Animator,
+                typeof (AnimatorComponent.Animator));
+        }
+
+    }
+
+}

0c8f83208f73acd739afda2b4a7518aa502e1a0e
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-04-12 16:06:18 +0200

    Update component menu

--- Editor/ComponentCreator.cs
+++ Editor/ComponentCreator.cs
@@ -1,5 +1,7 @@
 ﻿using ATP.AnimationPathTools.AnimatorComponent;
 using ATP.AnimationPathTools.AnimatorEventsComponent;
+using ATP.AnimationPathTools.AnimatorSynchronizerComponent;
+using ATP.AnimationPathTools.AudioSynchronizerComponent;
 using UnityEditor;
 
 namespace ATP.AnimationPathTools {
@@ -21,19 +23,34 @@ namespace ATP.AnimationPathTools {
             ScriptableObjectUtility.CreateAsset<AnimatorEventsSettings>("AnimatorEventsSettings");
         }
 
-        [MenuItem("Component/ATP/AnimationPathTools/Animator")]
-        private static void AddAPAnimatorComponent() {
+        [MenuItem("Component/ATP/AnimationPathTools/AnimationPathAnimator")]
+        private static void AddAnimationPathAnimatorComponent() {
             if (Selection.activeGameObject != null) {
                 Selection.activeGameObject.AddComponent(typeof(AnimationPathAnimator));
             }
         }
 
         [MenuItem("Component/ATP/AnimationPathTools/AnimatorEvents")]
-        private static void AddAPEventsComponent() {
+        private static void AddAnimatorEventsComponent() {
             if (Selection.activeGameObject != null) {
                 Selection.activeGameObject.AddComponent(typeof(AnimatorEvents));
             }
         }
+
+        [MenuItem("Component/ATP/AnimationPathTools/AnimatorSynchronizer")]
+        private static void AddAnimatorSynchronizerComponent() {
+            if (Selection.activeGameObject != null) {
+                Selection.activeGameObject.AddComponent(typeof(AnimatorSynchronizer));
+            }
+        }
+
+        [MenuItem("Component/ATP/AnimationPathTools/AudioSynchronizer")]
+        private static void AddAudioSynchronizerComponent() {
+            if (Selection.activeGameObject != null) {
+                Selection.activeGameObject.AddComponent(typeof(AudioSynchronizer));
+            }
+        }
+
     }
 
 }
